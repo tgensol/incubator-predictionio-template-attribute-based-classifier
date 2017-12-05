8f1ee584ba6988819f4e40d2fde2dccb07e2ab80
Thibaut Gensollen <thibaut.gensollen@ens-cachan.fr>
2017-12-05 15:12:43 +0100

    test

--- src/main/scala/DataSource.scala
+++ src/main/scala/DataSource.scala
@@ -12,103 +12,107 @@ import org.apache.spark.mllib.linalg.Vectors
 
 import grizzled.slf4j.Logger
 
-case class DataSourceParams(
-  appName: String,
-  evalK: Option[Int]  // define the k-fold parameter.
-) extends Params
+case class DataSourceParams(val appName: String) extends Params
 
 class DataSource(val dsp: DataSourceParams)
-  extends PDataSource[TrainingData,
+   extends PDataSource[TrainingData,
       EmptyEvaluationInfo, Query, ActualResult] {
 
   @transient lazy val logger = Logger[this.type]
 
   override
   def readTraining(sc: SparkContext): TrainingData = {
-
-    val labeledPoints: RDD[LabeledPoint] = PEventStore.aggregateProperties(
+  
+    val eventsRDD: RDD[Event] = PEventStore.find(
       appName = dsp.appName,
-      entityType = "user",
-      // only keep entities with these required properties defined
-      required = Some(List("plan", "attr0", "attr1", "attr2")))(sc)
-      // aggregateProperties() returns RDD pair of
-      // entity ID and its aggregated properties
-      .map { case (entityId, properties) =>
-        try {
-          LabeledPoint(properties.get[Double]("plan"),
-            Vectors.dense(Array(
-              properties.get[Double]("attr0"),
-              properties.get[Double]("attr1"),
-              properties.get[Double]("attr2")
-            ))
-          )
-        } catch {
-          case e: Exception => {
-            logger.error(s"Failed to get properties ${properties} of" +
-              s" ${entityId}. Exception: ${e}.")
-            throw e
-          }
-        }
-      }.cache()
+      entityType = Some("choose"),
+      eventNames = Some(List("contact"))
+    )(sc).cache()
 
+    val labeledPoints: RDD[TextClass] = eventsRDD
+      .filter {event => event.event == "contact"}
+      .map { event =>
+
+      try {
+        TextClass(
+          text_type = event.entityId,
+          text = event.properties.get[String]("text"),
+          replyTo = event.properties.getOpt[String]("replyTo"),
+          gender = event.properties.getOpt[Number]("gender"),
+          bdate = event.properties.getOpt[Number]("bdate"),
+          lang = event.properties.getOpt[String]("lang"),
+          platform = event.properties.getOpt[String]("platform")
+        ) 
+      } catch {
+        case e: Exception =>
+          logger.error(s"Cannot convert ${event} to TextClass." +
+            s" Exception: ${e}.")
+          throw e
+      }
+    }
+    
     new TrainingData(labeledPoints)
   }
-
   override
   def readEval(sc: SparkContext)
   : Seq[(TrainingData, EmptyEvaluationInfo, RDD[(Query, ActualResult)])] = {
-    require(dsp.evalK.nonEmpty, "DataSourceParams.evalK must not be None")
-
-    // The following code reads the data from data store. It is equivalent to
-    // the readTraining method. We copy-and-paste the exact code here for
-    // illustration purpose, a recommended approach is to factor out this logic
-    // into a helper function and have both readTraining and readEval call the
-    // helper.
-    val labeledPoints: RDD[LabeledPoint] = PEventStore.aggregateProperties(
+    val eventsRDD: RDD[Event] = PEventStore.find(
       appName = dsp.appName,
-      entityType = "user",
-      // only keep entities with these required properties defined
-      required = Some(List("plan", "attr0", "attr1", "attr2")))(sc)
-      // aggregateProperties() returns RDD pair of
-      // entity ID and its aggregated properties
-      .map { case (entityId, properties) =>
-        try {
-          LabeledPoint(properties.get[Double]("plan"),
-            Vectors.dense(Array(
-              properties.get[Double]("attr0"),
-              properties.get[Double]("attr1"),
-              properties.get[Double]("attr2")
-            ))
-          )
-        } catch {
-          case e: Exception => {
-            logger.error(s"Failed to get properties ${properties} of" +
-              s" ${entityId}. Exception: ${e}.")
-            throw e
-          }
-        }
-      }.cache()
-    // End of reading from data store
+      entityType = Some("choose"),
+      eventNames = Some(List("contact"))
+    )(sc).cache()
 
-    // K-fold splitting
-    val evalK = dsp.evalK.get
-    val indexedPoints: RDD[(LabeledPoint, Long)] = labeledPoints.zipWithIndex()
+    val labeledPoints: RDD[TextClass] = eventsRDD
+      .filter {event => event.event == "contact"}
+      .map { event =>
 
-    (0 until evalK).map { idx =>
-      val trainingPoints = indexedPoints.filter(_._2 % evalK != idx).map(_._1)
-      val testingPoints = indexedPoints.filter(_._2 % evalK == idx).map(_._1)
-
+      try {
+        TextClass(
+          text_type = event.entityId,
+          text = event.properties.get[String]("text"),
+          replyTo = event.properties.getOpt[String]("replyTo"),
+          gender = event.properties.getOpt[Number]("gender"),
+          bdate = event.properties.getOpt[Number]("bdate"),
+          lang = event.properties.getOpt[String]("lang"),
+          platform = event.properties.getOpt[String]("platform")
+        ) 
+      } catch {
+        case e: Exception =>
+          logger.error(s"Cannot convert ${event} to TextClass." +
+            s" Exception: ${e}.")
+          throw e
+      }
+    }.cache()
+    
+    (0 until 1).map { idx =>
+      val random = idx
       (
-        new TrainingData(trainingPoints),
+        new TrainingData(labeledPoints),
         new EmptyEvaluationInfo(),
-        testingPoints.map {
-          p => (Query(p.features(0), p.features(1), p.features(2)), ActualResult(p.label))
+        labeledPoints.map {
+          p => (new Query(p.text, p.replyTo, p.gender, p.bdate, p.lang, p.platform), 
+            new ActualResult(p.text_type))
         }
       )
     }
   }
 }
 
+
+case class TextClass(
+  val text_type: String,
+  val text: String,
+  val replyTo: Option[String],
+  val gender: Option[Number],
+  val bdate: Option[Number],
+  val lang: Option[String],
+  val platform: Option[String]
+)
+
 class TrainingData(
-  val labeledPoints: RDD[LabeledPoint]
-) extends Serializable
+  val texts: RDD[TextClass]
+) extends Serializable {
+  override def toString = {
+    s"queries: [${texts.count()}] (${texts.take(1).toList}...)"
+  }
+}
